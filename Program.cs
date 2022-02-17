775ac96c27ee9a3456484b49e354b6cf38188aee
kzlager <highcolour@yandex.ru>
2022-02-17 07:57:04 +0300

    save

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿// Задача 26: Возведите число А в натуральную степень B используя цикл
+
+Console.WriteLine("Возводим число А в натуральную степень В");
+Console.Write("a = ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("b = ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+double result = Math.Pow(a, b);
+Console.WriteLine($"{a} в степени {b} = {result}");
